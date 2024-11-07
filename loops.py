32204677a8f34dbc52e86be3d4bd35986a9bc79c
Saideekshitha <104785671+Saideekshithach@users.noreply.github.com>
2024-11-07 18:59:35 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,69 @@
+#for loop
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+a=[10,20,30,40,50,60]
+
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i,end=" ")'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+'''a=(4,5,6,7,8,9,10)
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))
+a={3,7,9,11,15}
+for i in a:
+    print(i)
+    print(type(a))
+
+a={"name":"pooja","place":"vja"}
+for i in a.keys():
+    print(i)
+for i in a.values():
+    print(i)
+for i in a.items():
+    print(i)
+    print(type(a))'''
+
+'''fruits=["mango","apple","kiwi","banana"]
+for i in fruits:
+    print(i,end=" ")'''
+
+'''names=["keerthana","geethika","sadhika","deekshitha"]
+for i in names:
+    print(i)'''
+'''a=[1,2,3,4,5,6]
+for i in a:
+    print(i,end=",")'''
+"""a=["vja","hyd","vzg"]
+for i in a:
+    print(i)"""
+#while loop
+'''a=10
+while a<1:
+    print(a)'''
+'''a=10
+while a>1:
+    print(a)'''
+'''a=10
+while a>5:
+    print(a)
+    a=a-1'''
+a=[2,3,4,5,6,7]
+b=int(input("enter the a value"))
+for b in a:
+    print("true")
+
+    
+
